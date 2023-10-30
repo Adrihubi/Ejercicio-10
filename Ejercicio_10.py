f89744966dc4c07af82bc853148ca2afe1e70a64
Cesar Adrian Zavala Perez <cesaradrizavperez@gmail.com>
2023-10-29 23:42:53 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio_10.py
@@ -0,0 +1,13 @@
+def sumaIterativa(n):
+    suma = 0
+
+    while n > 9:
+        suma += n % 10
+        n //= 10
+
+    return suma + n
+
+def main():
+    numero = 12345
+    resultado = sumaIterativa(numero)
+    print(f"La suma de los dígitos de {numero} es {resultado}")
